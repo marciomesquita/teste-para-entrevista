970bb43dbd520660607d38e04c595982bd730d77
Marcio Mesquita <botasebatatas@gmail.com>
2021-02-21 20:06:11 -0300

    Inicial

---
+++ TesteParaEntrevista/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TesteParaEntrevista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(args);
+        }
+    }
+}
